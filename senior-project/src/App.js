dd85541c746609f0dc86996138680a75fb1382d0
Chris Zuelke <zuelkechristopher@gmail.com>
2023-05-03 11:37:39 -0400

    Backup

--- senior-project/src/App.js
+++ senior-project/src/App.js
@@ -1,64 +1,57 @@
-//TODO: add user images gpt, user
-//TODO: format to look better
-//TODO: chat history - localstorage/cookies
-//TODO: local storage api key/user - localstorage/cookies
-//TODO: add to git host
-//TODO: mobile version
-//TODO: Connect to API
-//TODO: envirmental key - remove old api key
-
-
-
 //import logo from './logo.svg';
 import './App.css';
 import './normal.css';
-import { useState, userstate } from 'react';
+//import { useState } from 'react';
 
 function App() {
-
-  const [input, setInput] = useState("");
-  const [chatLog, setChatLog] = useState([]);
-
-
-  async function submitInput(e){
-    e.preventDefault();
-    setChatLog([...chatLog, {user: "me", message: `$(input)`}])
-    setInput="";
-
-
-  }
-
-
-
   return (
     <div className="App">
-      <aside className="classmenu">
-        <h1>Menu</h1>
-        <div className="newChatButton">
-          <span>+ New Chat</span>
+      <aside className='sidemenu'>
+        <h1>menu</h1>
+        <div className='side-menu-button'>
+          + New Chat
         </div>
       </aside>
-      <section className="inputbox">
-      <div className="messages">
-        <div className="chatMessage">
-          hello
+
+      <section className='chatbox'>
+        <div className='chat-log'>
+
+          <div className='chat-message'>
+            <div className='chat-message-center'>
+              <div className='avatar chatgpt'>
+              </div>
+              <div className='message'>
+                Hello
+              </div>
+            </div>
+          </div>
+
+          <div className='chat-message chatgpt'>
+            <div className='chat-message-center'>
+              <div className='avatar'>
+                <img src="https://chat.openai.com/favicon-32x32.png" height='40' alt='GPT'/>
+              </div>
+              <div className='message'>
+                The AI
+              </div>
+            </div>
+          </div>
+
         </div>
-      </div>
-      <div className="messages AI">
-        <div className="chatMessage">
-          the ai says hello
+
+        <div className='chat-input-holder'>
+          <textarea 
+          rows="1"
+          className='chat-input-textarea' 
+          placeholder='Type Your Question'>
+          </textarea>
         </div>
-      </div>
-      <div className="chatInput">
-        <form onSubmit={submitInput}>
-          <input className="input" value={input} 
-          onChange={(e)=> setInput(e.target.value)} placeholder="Type your question here" rows="1"></input>
-        </form>
-      </div>
+
       </section>
     </div>
-  );
+  )
 }
 
-
 export default App;
+
+
